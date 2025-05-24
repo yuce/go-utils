41861a2fbd03bd68ea959d71ae1ed5a48732322d
Yuce Tekol <yucetekol@gmail.com>
2025-05-24 11:44:20 +0300

    Added the SplitByComma method

--- strs/strings.go
+++ strs/strings.go
@@ -1,5 +1,7 @@
 package strs
 
+import "strings"
+
 // FirstNonEmpty returns the first non-empty string from the given strings.
 func FirstNonEmpty(items ...string) string {
 	for _, item := range items {
@@ -8,4 +10,19 @@ func FirstNonEmpty(items ...string) string {
 		}
 	}
 	return ""
+}
+
+// SplitByComma splits the text by comma.
+// Each part is trimmed, empty parts are kept.
+// If a string is all-whitespaces, then nil is returned.
+func SplitByComma(text string) []string {
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return nil
+	}
+	items := strings.Split(text, ",")
+	for i, item := range items {
+		items[i] = strings.TrimSpace(item)
+	}
+	return items
 }
--- strs/strings_test.go
+++ strs/strings_test.go
@@ -2,9 +2,10 @@ package strs_test
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/yuce/go-utils/assert"
 	"github.com/yuce/go-utils/strs"
-	"testing"
 )
 
 func TestFirstNonEmpty(t *testing.T) {
@@ -37,6 +38,46 @@ func TestFirstNonEmpty(t *testing.T) {
 		t.Run(fmt.Sprint(tc.Strings), func(t *testing.T) {
 			o := strs.FirstNonEmpty(tc.Strings...)
 			assert.StringEqual(tc.Want, o)
+		})
+	}
+}
+
+func TestSplitByComma(t *testing.T) {
+	testCases := []struct {
+		Name   string
+		Text   string
+		Result []string
+	}{
+		{
+			Name:   "empty string",
+			Text:   "",
+			Result: nil,
+		},
+		{
+			Name:   "single item",
+			Text:   "foo",
+			Result: []string{"foo"},
+		},
+		{
+			Name:   "two items",
+			Text:   "foo,bar",
+			Result: []string{"foo", "bar"},
+		},
+		{
+			Name:   "two items with space around comma",
+			Text:   "foo, bar",
+			Result: []string{"foo", "bar"},
+		},
+		{
+			Name:   "three items",
+			Text:   "foo, bar, quux",
+			Result: []string{"foo", "bar", "quux"},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			r := strs.SplitByComma(tc.Text)
+			assert.SliceEqual(tc.Result, r)
 		})
 	}
 }
